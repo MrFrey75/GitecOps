aad0324e5c76f28e4461be39efcaa9e96a071700
Arthur Frey <afrey@gi-tec.net>
2025-05-15 09:48:43 -0400

    feat: Refactor Initialize-GitecModules function for improved module management in DailyRun, StartUp, and WeeklyRun scripts
    push it

--- scripts/modules/CoreHelper.psm1
+++ scripts/modules/CoreHelper.psm1
@@ -1,26 +1,3 @@
-function Initialize-GitecModules {
-    param (
-        [Parameter(Mandatory = $true)][string]$BaseDir
-    )
-
-    $script:scriptDirectory     = Join-Path $BaseDir "scripts"
-    $script:moduleDirectory     = Join-Path $scriptDirectory "modules"
-    $script:assetsDirectory     = Join-Path $scriptDirectory "assets"
-    $script:loggingModulePath   = Join-Path $moduleDirectory "LoggingHelper.psm1"
-    $script:utilityModulePath   = Join-Path $moduleDirectory "Utilities.psm1"
-    $script:registryModulePath  = Join-Path $moduleDirectory "RegistryHelper.psm1"
-    $script:deviceModulePath    = Join-Path $moduleDirectory "DeviceHelper.psm1"
-
-    if (-not ($env:PSModulePath -split ";" | Where-Object { $_ -eq $moduleDirectory })) {
-        $env:PSModulePath = "$moduleDirectory;$env:PSModulePath"
-    }
-
-    Import-Module -Name $utilityModulePath   -Force -ErrorAction Stop
-    Import-Module -Name $loggingModulePath   -Force -ErrorAction Stop
-    Import-Module -Name $registryModulePath  -Force -ErrorAction Stop
-    Import-Module -Name $deviceModulePath    -Force -ErrorAction Stop
-}
-
 function Set-RunTimestamp {
     param (
         [Parameter(Mandatory = $true)][string]$RegKey
@@ -30,6 +7,17 @@ function Set-RunTimestamp {
         Write-Info "Registry key '$RegKey' set successfully."
     } catch {
         Write-Error "Error setting registry key '$RegKey': $_"
+    }
+}
+
+function Clear-TempFiles {
+    Write-Host "Clearing temporary files..."
+    $tempPath = [System.IO.Path]::GetTempPath()
+    try {
+        Get-ChildItem -Path $tempPath -Recurse -Force | Remove-Item -Force -Recurse
+        Write-Info "Temporary files cleared successfully."
+    } catch {
+        Write-Host "Failed to clear temporary files: $_" -ForegroundColor Yellow
     }
 }
 
